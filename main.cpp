ec2ec5bbe2dda5c99029158934c80477aa1b0cb9
deyanwesha15 <anweshadeystudent@gmail.com>
2026-07-18 20:29:17 +0600

    Add student information display in main.cpp Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "===== Student Information =====" << endl;
+    cout << "Name: Anwesha Dey" << endl;
+    cout << "Student ID: 24-58944-2" << endl;
+    cout << "Section: X" << endl;
+    cout << "Favorite Programming Language: C++" << endl;
+
+    return 0;
+}
